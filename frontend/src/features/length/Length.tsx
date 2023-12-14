38efd50dffee1e1c47bdc1ee5dd1e1a8bae32028
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-12-14 16:49:36 +0100

    added redux for length

--- frontend/src/features/length/Length.tsx
+++ frontend/src/features/length/Length.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/types";
 
-export const Length: React.FC = () => {
+export const Length = () => {
   const totalLength = useSelector((state: RootState) => state.totalLength);
 
   const formatPxToMb = (nbInPixels: number) => {
@@ -15,7 +15,7 @@ export const Length: React.FC = () => {
 
   return (
     <>
-      {/* <p>Całkowita długość w pikselach: {totalLength} pikseli</p> */}
+      <p>Całkowita długość w pikselach: {totalLength} pikseli</p>
       <p>Całkowita długość w metrach: {formatPxToMb(totalLength)} metrów</p>
     </>
   );
