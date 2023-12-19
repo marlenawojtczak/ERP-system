82f41a29f7445648579b67c37a41d24e56fc5b89
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-12-19 23:19:16 +0100

    added form inputs

--- frontend/src/features/form/FormComponent.styled.tsx
+++ frontend/src/features/form/FormComponent.styled.tsx
@@ -1,32 +1,95 @@
 import styled from "styled-components";
 
+interface TextAreaProps {
+  type?: string;
+}
+interface PriorityButtonProps {
+  isActive: boolean;
+}
+
 export const MainForm = styled.form`
   display: flex;
 `;
 
 export const PrintableForm = styled.div`
-  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen {
+    padding: 24px;
+  }
+  @media print {
+    width: 100%;
+    margin: auto;
+    padding: 2cm;
+    margin-left: -240px;
+    margin-top: -60px;
+    @page {
+      margin: 1cm;
+      margin-top: 0;
+      margin-bottom: 0;
+      /* size: A4; */
+    }
+  }
 `;
 
 export const NoPrintableForm = styled.div`
   height: calc(100vh - 120px);
   border-left: 2px solid black;
   padding: 24px;
+
+  @media print {
+    display: none;
+  }
 `;
 
 export const FormGroup = styled.div`
   margin-bottom: 8px;
+  display: flex;
 `;
 
 export const Label = styled.label`
   display: block;
   margin-bottom: 2px;
+  width: 150px;
 `;
 
 export const Input = styled.input`
-  width: 60%;
+  width: 250px;
   padding: 4px;
   font-size: 12px;
+`;
+
+export const TextArea = styled.textarea<TextAreaProps>`
+  width: 250px;
+  padding: 4px;
+  font-size: 12px;
+  resize: none;
+`;
+
+export const RadioGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-right: 12px;
+`;
+export const RadioInput = styled.input`
+  padding: 4px;
+  font-size: 12px;
+`;
+export const RadioLabel = styled.label`
+  display: block;
+  margin-bottom: 2px;
+`;
+
+export const PriorityButton = styled.button<PriorityButtonProps>`
+  padding: 10px;
+  font-size: 16px;
+  background-color: ${({ isActive }) => (isActive ? "#dc4c64" : "white")};
+  color: ${({ isActive }) => (isActive ? "white" : "black")};
+  border: none;
+  cursor: pointer;
+  width: 100px;
 `;
 
 export const SubmitButton = styled.button`
@@ -36,8 +99,29 @@ export const SubmitButton = styled.button`
   color: #fff;
   border: none;
   cursor: pointer;
+  width: 80%;
 
   &:hover {
     background-color: #45a049;
+  }
+
+  @media print {
+    display: none;
+  }
+`;
+
+export const ResetButton = styled.button`
+  margin-left: 10px;
+  padding: 10px;
+  font-size: 16px;
+  background-color: white;
+  color: #dc4c64;
+  border: none;
+  cursor: pointer;
+  width: 100px;
+
+  &:hover {
+    background-color: #dc4c64;
+    color: white;
   }
 `;
--- frontend/src/features/form/FormComponent.tsx
+++ frontend/src/features/form/FormComponent.tsx
@@ -7,6 +7,12 @@ import {
   FormGroup,
   Label,
   Input,
+  TextArea,
+  RadioGroup,
+  RadioInput,
+  RadioLabel,
+  PriorityButton,
+  ResetButton,
   SubmitButton,
 } from "./FormComponent.styled";
 
@@ -18,12 +24,25 @@ interface FormData {
   roll: string;
   length: number;
   welds: string;
+  email: string;
+  phone: string;
+  mailbox: string;
+  invoice: string;
+  nip: string;
+  address: string;
+  shipmentAddress: string;
+  paczkomat: string;
+  cost: string;
+  comments: string;
+  priority: boolean;
 }
 
 export const FormComponent: React.FC = () => {
   const [localTotalLength, setLocalTotalLength] = useState<number>(0);
+  const [files, setFiles] = useState([]);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
-  const [formData, setFormData] = useState<FormData>({
+  const InitialState = {
     name: "",
     order: "",
     deadline: "",
@@ -31,7 +50,20 @@ export const FormComponent: React.FC = () => {
     roll: "",
     length: localTotalLength,
     welds: "",
-  });
+    email: "",
+    phone: "",
+    mailbox: "",
+    invoice: "",
+    nip: "",
+    address: "",
+    shipmentAddress: "",
+    paczkomat: "",
+    cost: "",
+    comments: "",
+    priority: false,
+  };
+
+  const [formData, setFormData] = useState<FormData>(InitialState);
 
   useEffect(() => {
     setFormData((prevData) => ({
@@ -40,7 +72,9 @@ export const FormComponent: React.FC = () => {
     }));
   }, [localTotalLength]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -48,14 +82,33 @@ export const FormComponent: React.FC = () => {
     }));
   };
 
+  const handleTogglePriority = () => {
+    setFormData((prevData) => ({
+      ...prevData,
+      priority: !prevData.priority,
+    }));
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
+
+  const handlePrint = () => {
+    window.print();
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // You can perform form submission logic here, e.g., send data to a server
     console.log("Form submitted:", formData);
+    handlePrint();
+  };
+
+  const handleReset = () => {
+    setFormData(InitialState);
+    setIsActive(false);
+    setLocalTotalLength(0);
+    setFiles([]);
   };
 
   const formatPxToMb = (nbInPixels: number) => {
-    const nbInMeters = Number(nbInPixels * 0.000091).toLocaleString(undefined, {
+    const nbInMeters = Number(nbInPixels * 0.000085).toLocaleString(undefined, {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
@@ -69,6 +122,8 @@ export const FormComponent: React.FC = () => {
           <DropBox
             localTotalLength={localTotalLength}
             setLocalTotalLength={setLocalTotalLength}
+            files={files}
+            setFiles={setFiles}
           />
           <FormGroup>
             <Label htmlFor="name">Nazwa:</Label>
@@ -140,10 +195,216 @@ export const FormComponent: React.FC = () => {
               onChange={handleInputChange}
             />
           </FormGroup>
-          <SubmitButton type="submit">Submit</SubmitButton>
         </PrintableForm>
         <NoPrintableForm>
-          <p>TESTING2</p>
+          <FormGroup>
+            <Label htmlFor="email">email:</Label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="phone">nr telefonu:</Label>
+            <Input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="mailbox">Skrzynka:</Label>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="mailbox_option1"
+                name="mailbox"
+                value="mailbox_option1"
+                checked={formData.mailbox === "mailbox_option1"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="mailbox_option1">Zamówienia</RadioLabel>
+            </RadioGroup>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="mailbox_option2"
+                name="mailbox"
+                value="mailbox_option2"
+                checked={formData.mailbox === "mailbox_option2"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="mailbox_option2">Sklep</RadioLabel>
+            </RadioGroup>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="mailbox_option3"
+                name="mailbox"
+                value="mailbox_option3"
+                checked={formData.mailbox === "mailbox_option3"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="mailbox_option3">fb</RadioLabel>
+            </RadioGroup>
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="shipment">Wysyłka:</Label>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="shipment_option1"
+                name="shipment"
+                value="Kurier"
+                checked={formData.shipment === "Kurier"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="shipment_option1">Kurier</RadioLabel>
+            </RadioGroup>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="shipment_option2"
+                name="shipment"
+                value="Paczkomat"
+                checked={formData.shipment === "Paczkomat"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="shipment_option2">Paczkomat</RadioLabel>
+            </RadioGroup>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="shipment_option3"
+                name="shipment"
+                value="Osobiście"
+                checked={formData.shipment === "Osobiście"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="shipment_option3">Osobiście</RadioLabel>
+            </RadioGroup>
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="invoice">Faktura:</Label>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="invoice_option1"
+                name="invoice"
+                value="Tak"
+                checked={formData.invoice === "Tak"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="invoice_option1">Tak</RadioLabel>
+            </RadioGroup>
+            <RadioGroup>
+              <RadioInput
+                type="radio"
+                id="invoice_option2"
+                name="invoice"
+                value="Nie"
+                checked={formData.invoice === "Nie"}
+                onChange={handleInputChange}
+              />
+              <RadioLabel htmlFor="invoice_option2">Nie</RadioLabel>
+            </RadioGroup>
+          </FormGroup>
+          <p>Dane do faktury:</p>
+          <FormGroup>
+            <Label htmlFor="nip">NIP:</Label>
+            <Input
+              type="text"
+              id="nip"
+              name="nip"
+              value={formData.nip}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="address">Adres:</Label>
+            <Input
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="name">Nazwa firmy:</Label>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <p>Dane do wysyłki:</p>
+          <FormGroup>
+            <Label htmlFor="shipmentAddress">Adres:</Label>
+            <Input
+              type="text"
+              id="shipmentAddress"
+              name="shipmentAddress"
+              value={formData.shipmentAddress}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label htmlFor="paczkomat">Nr Paczkomatu:</Label>
+            <Input
+              type="text"
+              id="paczkomat"
+              name="paczkomat"
+              value={formData.paczkomat}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="cost">Koszt:</Label>
+            <Input
+              type="text"
+              id="cost"
+              name="cost"
+              value={formData.cost}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="comments">Uwagi:</Label>
+            <TextArea
+              type="text"
+              id="comments"
+              name="comments"
+              value={formData.comments}
+              onChange={handleInputChange}
+              rows={5}
+            >
+              Komentarz
+            </TextArea>
+          </FormGroup>
+
+          <PriorityButton
+            type="button"
+            id="priority"
+            onClick={handleTogglePriority}
+            isActive={isActive}
+          >
+            {formData.priority ? "Pilne on" : "Pilne off"}
+            {/* Pilne */}
+          </PriorityButton>
+          <ResetButton type="button" onClick={handleReset}>
+            Reset Form
+          </ResetButton>
+
+          <SubmitButton type="submit">Utwórz zamówienie</SubmitButton>
         </NoPrintableForm>
       </MainForm>
     </>
