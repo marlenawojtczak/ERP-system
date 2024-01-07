eec2293f1a75844962b3bdeb6cf01e94966e27cb
Roland Dąbrowski <roli.dabrowski@gmail.com>
2024-01-07 18:49:03 +0100

    edited order schema

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -10,7 +10,7 @@ export class OrdersController {
   @Public()
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
-  createOrder(@Body() dto: OrderDto): Promise<any> {
+  createOrder(@Body() dto: OrderDto): Promise<OrderDto> {
     return this.ordersService.createOrder(dto);
   }
 }
