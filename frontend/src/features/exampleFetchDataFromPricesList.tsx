15764f8d7126e8a4133d8b4f6c1fe11dc61998aa
marlenawojtczak <wojtczakmarlena100@gmail.com>
2024-01-08 19:13:34 +0100

    set fetching without auth

--- frontend/src/features/exampleFetchDataFromPricesList.tsx
+++ frontend/src/features/exampleFetchDataFromPricesList.tsx
@@ -18,12 +18,7 @@ export const PriceCalculator = () => {
 
   const fetchPrices = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/prices", {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NThkNjhiYzU1NjJkYjBkMzY3ZjU0ZjEiLCJlbWFpbCI6Im1hcmxlbmFAZ21haWwuY29tIiwiaWF0IjoxNzA0NjM2NTUyLCJleHAiOjE3MDQ3MjY1NTJ9.r2Rxwc79FAkqe1-aipTO9qjWgvJtc5Qz5f2SSX-A9IQ`,
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch("http://localhost:3000/api/prices");
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
