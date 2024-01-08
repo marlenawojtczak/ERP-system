510e1d6be3774dea9938c62270ff90170a433902
Roland Dąbrowski <roli.dabrowski@gmail.com>
2024-01-08 21:41:30 +0100

    added endpoints orders

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrderDto } from './dto';
 import { Public } from 'src/common/decorators';
@@ -10,7 +18,21 @@ export class OrdersController {
   @Public()
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
-  createOrder(@Body() dto: OrderDto): Promise<OrderDto> {
+  createOrder(@Body() dto: OrderDto) {
     return this.ordersService.createOrder(dto);
+  }
+
+  @Public()
+  @Get('/')
+  @HttpCode(HttpStatus.OK)
+  getAllOrder(): Promise<any> {
+    return this.ordersService.getAllOrders();
+  }
+
+  @Public()
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  getOrderById(@Param('id') id: string): Promise<any> {
+    return this.ordersService.getOrderById(id);
   }
 }
--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { OrderDto } from './dto';
 
 @Injectable()
 export class OrdersService {
   constructor(private prisma: PrismaService) {}
 
-  async createOrder(data: any): Promise<OrderDto> {
+  async createOrder(data: any) {
     return await this.prisma.order.create({
       data: {
         ...data,
+      },
+    });
+  }
+
+  async getAllOrders(): Promise<any> {
+    return await this.prisma.order.findMany();
+  }
+
+  async getOrderById(id: string): Promise<any> {
+    return await this.prisma.order.findUnique({
+      where: {
+        id,
       },
     });
   }
